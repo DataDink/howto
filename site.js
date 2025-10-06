f6941a91bba68ac9bf49146eb50687d0e882e311
Greenwald <Greenwald@Greenwald>
2025-10-06 07:46:41 -0700

    responsive nav toggle

--- site.js
+++ site.js
@@ -1,3 +1,4 @@
 import INLINEIMPORT from './components/inline-import.js';
 import SRCNAV from './components/src-nav.js';
 import CODESAMPLE from './components/code-sample.js';
+import TOGGLEBLOCK from './components/toggle-button.js';
